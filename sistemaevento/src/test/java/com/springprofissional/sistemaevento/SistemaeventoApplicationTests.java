5dcf80bb77628ea237a09a09f557d8fa4adc02d5
Gustavo Camargo Da Silva <gustavocamargodasilva@hotmail.com>
2023-11-23 16:54:16 -0300

    Criacao do projeto para exercicio de eventos

---
+++ sistemaevento/src/test/java/com/springprofissional/sistemaevento/SistemaeventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springprofissional.sistemaevento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaeventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
