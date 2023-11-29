8050d6c4231c4e83e385d0fb004a0d254ed5d077
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2023-11-29 18:31:07 -0300

    metodo post insert novo produto

--- dscommerce/src/main/java/com/springprofissional/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/springprofissional/dscommerce/controllers/ProductController.java
@@ -7,9 +7,12 @@ import com.springprofissional.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.swing.text.html.Option;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,18 +23,22 @@ public class ProductController {
     @Autowired
     private ProductService service;
     @GetMapping(value = "/{id}") //esse id vai casar com o parametro do metodo
-    public ProductDTO findById(@PathVariable Long id){
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id){ //retornar um reponseentity do tipo dto
         ProductDTO dto = service.findById(id);
-        return dto;
+        return ResponseEntity.ok(dto); //pontoOK vai retornar o codigo 201 que é o correto
     }
 
     @GetMapping //get de todos os produtos da lista paginados
-    public Page<ProductDTO> findAll(Pageable pageable){ //Pageable quer dizer que ele vai me retornar os resultados paginados
-        return service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){ //Pageable quer dizer que ele vai me retornar os resultados paginados
+        Page<ProductDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
     }
     @PostMapping //metodo para criar um produto
-    public ProductDTO insert(@RequestBody ProductDTO dto){ //RequestBody faz com que o corpo correspondente do json corresponda e instancie um dto quando chamado o metodo
-       return service.insert(dto);
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){ //RequestBody faz com que o corpo correspondente do json corresponda e instancie um dto quando chamado o metodo
+       dto = service.insert(dto);
+       URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+               .buildAndExpand(dto.getId()).toUri();
+       return ResponseEntity.created(uri).body(dto);
     }
 
 
