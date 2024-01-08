425130e3bfc20275345283ed146ea538711dda75
Gustavo Camargo <104240187+GustavoCamargodaSilva@users.noreply.github.com>
2024-01-08 10:03:03 -0300

    criaçao projections

--- desafio-consulta-vendas/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ desafio-consulta-vendas/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,14 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.projections.SaleMinProjection;
+import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -19,5 +25,24 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleMinDTO> searchPagedSalesWithInitialDateFinalDateAnSellerName(
+			String minDateStr, String maxDateStr, String name, Pageable pageable){
+
+		LocalDate minDate;
+		LocalDate maxDate;
+
+		try {
+			minDate = LocalDate.parse(minDateStr);
+			maxDate = LocalDate.parse(maxDateStr);
+		}catch (DateTimeException e){
+			throw new IllegalArgumentException("Formato de data está inválido.");
+		}
+
+		Page<SaleMinProjection> saleMinProjectionPage = repository
+				.searchByInitialDateFinalDateAndSellerPartialName(minDate, maxDate, name, pageable);
+		Page<SaleMinDTO> res = saleMinProjectionPage.map(x -> new SaleMinDTO(x));
+		return res;
 	}
 }
